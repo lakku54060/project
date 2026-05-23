30a27b665a870ddbad324996da6fb6e1a8084494
Lakshya Gangwal <lakku54060@gmail.com>
2026-05-23 15:57:18 -0700

    Fix case-sensitive CSS import for Vercel build

--- src/Adminlogin.jsx
+++ src/Adminlogin.jsx
@@ -1,8 +1,8 @@
-﻿import axios from "axios";
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiUrl } from "./utils/api";
-import "./Adminlogin.css";
+import "./adminlogin.css";
 
 function MailIcon() {
   return <svg viewBox="0 0 24 24"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>;
